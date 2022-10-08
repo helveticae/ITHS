d1f3563cae970ddaef962c694af02b2f212bb2b9
WL <w.lindstedt@gmail.com>
2022-10-08 19:48:05 +0200

    reverse string function

---
+++ Explorations/reverse_string.py
@@ -0,0 +1,8 @@
+# Simple string reverser
+
+def reverse_string(str:str) -> str:
+    """Reverses string using slicing"""
+    return str[::-1]
+
+if __name__ == "__main__":
+    print(reverse_string("tardavk ne rä nesnatsnilegnatker mo rakcehc mos dotem ne lekricstehne ne rä nesnatsnilekric mo rakcehc mos dotem ne gniretnahlef tetkejbo i röfnanni gis rennifeb tknup ssiv ne mo rakcehc mos dotem ne y hco x attylfröf tta tgiljöm ted rög mos dotemsnoitalsnart ne tetkejbo va nenoitisopttim raretneserper mos y hco x va edirrevo ne va edirrevo ne rerotareporotarapmok va daolrevo rotarepo ne tehkil akcehc tta röf va daolrevo rotarepo ne ytreporp sterkmo ne ytreporp aera ne resleröfmäj röf :ah aks anressalK"))
