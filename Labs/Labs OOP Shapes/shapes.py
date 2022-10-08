010766cd7b2160174f483df0a95a563eacfdd8d3
WL <w.lindstedt@gmail.com>
2022-10-08 20:22:58 +0200

    add two child class to shape class

--- Labs/Labs OOP Shapes/shapes.py
+++ Labs/Labs OOP Shapes/shapes.py
@@ -1,12 +1,37 @@
+from __future__ import annotations
+
 #TODO finish before deadline
 
+"""
+Class should have:
 
+area property
+circumference property
 
+operator overload av == för att checka likhet
+operator overload av komparatoroperatorer <,>,<=>,> för jämförelser
+override av __repr__()
+override av __str__
 
+x och y som representerar mittpositionen av objektet
+en translationsmetod som gör det möjligt att förflytta x och y
+en metod som checkar om en viss punkt befinner sig innanför objektet
+felhantering
+en metod som checkar om cirkelinstansen är en enhetscirkel
+en metod som checkar om rektangelinstansen är en kvadrat
 
-class Square:
+"""
+
+class Shape:
+    def __init__(self, area: float | int, circumference: float | int, position: tuple) -> None:
+        self.area = area
+        self.circumference = circumference
+        self.position = position
+
+class Square(Shape):
     pass
 
-class Circle:
+class Circle(Shape):
+    pass
 
-class 
+square1 = Square(25.0,20.0,[0.0,0.0])
