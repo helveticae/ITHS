e6c0258d0ee864f624066320e6ed0dbf9a788be1
WL <w.lindstedt@gmail.com>
2022-10-11 22:37:53 +0200

    better docstring, add circle attributes and func

--- Labs/Labs OOP Shapes/geometry_shapes.py
+++ Labs/Labs OOP Shapes/geometry_shapes.py
@@ -5,48 +5,48 @@ import math
 #TODO finish before deadline
 
 """
-Class should have:
-
-area property (Done for rectangle)
-circumference property
-
-operator overload av == för att checka likhet
-
-operator overload av komparatoroperatorer <,>,<=>,> för jämförelser
-override av __repr__()
 override av __str__
 
 x och y som representerar mittpositionen av objektet
-en translationsmetod som gör det möjligt att förflytta x och y
+
 en metod som checkar om en viss punkt befinner sig innanför objektet
 felhantering
-en metod som checkar om cirkelinstansen är en enhetscirkel
-en metod som checkar om rektangelinstansen är en kvadrat
 
 """
 
-
-
-
-# Refactoring with @dataclass and int
+# Refactoring with @dataclass
 
 #TODO Make this @abstract instead (?)
 
-@dataclass
+@dataclass(repr=False)
 class Shape:
+    """
+    Parent class.
+    
+    An abstract(?) class. Shouldn't be instantiated on its own.
+
+    Attributes
+    ----------
+    position : tuple
+        Values representing the center position of the object (x,y,z.. etc).
+
+    Methods
+    ----------
+    translate_position(position: tuple)
+        Call as normal class method to update positional data of given shape.
+
+    """
+
     position: tuple
 
-    # Read-only @property, getters
-    @property
+    @property # Read-only, getter
     def position(self) -> tuple:
-
-        print("pos getter running")
-
+        print("position getter running")
         return self._position
 
-    # Setter
-    @position.setter
-    def position(self, value):
+    @position.setter # Read and write, setter
+    def position(self, value: tuple):
+        """Sets position to given value."""
 
         print("position setter running")
 
@@ -54,10 +54,63 @@ class Shape:
             raise TypeError(f"Position must be tuple not {type(value)}")
         self._position = value
 
+
+    def translate_position(self, position: tuple) -> tuple:
+        """
+        Callable method to translate positional values (center point) of shape.
+
+        Same as typing 'object.position = (tuple)' except translate_position()
+        stores old positional values in variable 'old_pos'.
+
+        """
+
+        print("translate_position running")
+
+        # Stores previous position
+        old_pos = self.position
+                # TODO: add meaning to this variable (maybe animating path or something)
+
+        # Updates position to new value
+        self.position = position
+
+        print(f"{old_pos=}, new={position}")
+
+        return self.position
+        
+    def __repr__(self):
+        return f"{self.__class__.__name__}, midpoint at {self.position}.)"
+
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
 
 class Rectangle(Shape):
-    """Rectangle. Inherits from Shape."""
+    """
+    Child class of Shape.
+    
+    Contains width, height, area and perimeter.
+
+    Attributes
+    ----------
+    width: float
+        Width (x) of rectangle.
+
+    height: float
+        Height (y) of rectangle.
+
+    area: float
+        Area of rectangle.
+
+    perimeter: float
+        Perimeter of rectangle.
+
+    Methods
+    -------
+    is_square(width: float, height: float)
+       Return True if width == height.
+
+    overloaded __eq__, __lt__, __le__, __gt__, __ge__
+
+    """
+
     def __init__(self, position: tuple, width: float, height: float) -> None:
         
         super().__init__(position)
@@ -72,36 +125,68 @@ class Rectangle(Shape):
     def perimeter(self):
         return self.width * 2 + self.height *2
 
+    def is_square(self, width: float, height: float) -> bool:
+        """Checks if given rectangle is square."""
+
+        if self.width == self.height:
+            return True
+        else: return False
 
     # Operator overloading for size equality.
-    def __eq__(self, other: Shape):
-        """Checks if shapes have identical size."""
+    def __eq__(self, other: Shape) -> bool:
+        """Checks if any two given rectangles are identical in size."""
+
         if self.width == other.width and self.height == other.height:
             return True
         else: return False
 
     # Operator overloading for area comparison
-    def __lt__(self, other) -> bool:
+    def __lt__(self, other: Shape) -> bool:
         if self.Area < other.Area: return True
         else: return False
         
-    def __le__(self, other) -> bool:
+    def __le__(self, other: Shape) -> bool:
         if self.Area <= other.Area: return True
         else: return False
 
-    def __gt__(self, other) -> bool:
+    def __gt__(self, other: Shape) -> bool:
         if self.Area > other.Area: return True
         else: return False
         
-    def __ge__(self, other) -> bool:
+    def __ge__(self, other: Shape) -> bool:
         if self.Area >= other.Area: return True
         else: return False
+
+    def __repr__(self):
+        return f"{self.__class__.__name__} -- midpoint at {self.position} -- {self.area=}.)"
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
 
 
 class Circle(Shape):
-    """Circle. Inherits from Shape."""
+    """
+    Child class of Shape.
+    
+    Contains radius, area and circumference.
+
+    Attributes
+    ----------
+    width: float
+        Width (x) of circle.
+
+    height: float
+        Height (y) of circle.
+
+    area: float
+        Area of circle.
+
+    circumference: float
+        Circumference of circle.
+
+    Methods
+    -------
+
+    """
     def __init__(self, position: tuple, radius: float) -> None:
         
         super().__init__(position)
@@ -110,11 +195,15 @@ class Circle(Shape):
     @property
     def area(self):
         return math.pi * self.radius ** 2
+    
+    @property
+    def circumference(self):
+        return math.pi * self.radius * 2
 
 
-# square1 = Rectangle((0,0),12,12)
-# square2 = Rectangle((0,0),12,11)
+    def is_unit_circle(self, width: float, height: float) -> bool:
+        """Should check if given circle is unit."""
+        return True
 
-# print(square1.area < square2.area)
-
-#square2 = Rectangle((0,0),2,2)
+    def __repr__(self):
+        return f"{self.__class__.__name__} -- midpoint at {self.position} -- {self.radius=}.)"
