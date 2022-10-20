a712e1eb78a983895c467eabfb59052f19f13389
William L <41084946+helveticae@users.noreply.github.com>
2022-10-20 15:35:56 +0200

    Initial commit

--- Explorations/rgb_to_hex.py
+++ Explorations/rgb_to_hex.py
@@ -1,10 +1,8 @@
 # Simple rgb to hex converter
 
-def rgb_to_hex(r: int, g:int ,b: int) -> str:
+def rgb_to_hex(r: int, g:int, b: int) -> str:
     """Converting three RGB values to their hex representation using :X while returning the hex value."""
-    return ('{:X}{:X}{:X}').format(r, g, b)
-
+    return (('{:X}{:X}{:X}').format(r, g, b).lower())
 
 if __name__ == "__main__":
-    a = rgb_to_hex(13, 17, 23)
-    print(a)
+    print(rgb_to_hex(143, 171, 121))
